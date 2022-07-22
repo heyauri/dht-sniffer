d40a65b44f2dee004d5e81d1cc9bc8d70b1ed5aa
叫我小朋友 <alexchan1st@163.com>
2022-07-22 16:40:02 +0800

    op

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,16 @@
-const {DHTSniffer} = require("../lib/dht-sniffer");
+const { DHTSniffer } = require("../lib/dht-sniffer");
 
-let sniffer = new DHTSniffer({port:23456})
-
-sniffer.on("infoHash",infoHash=>{
-    console.log("get infoHash:",infoHash)
+let sniffer = new DHTSniffer({ port: 23456, refreshTime: 3000 })
+sniffer.start();
+sniffer.on("infoHash", infoHash => {
+    console.log("get infoHash:", infoHash)
+})
+sniffer.on("node", node => {
+    console.log("find node", node)
+})
+sniffer.on("warning", err => {
+    console.error(err);
+})
+sniffer.on("error", err => {
+    console.error(err);
 })
--- src/dht-sniffer.ts
+++ src/dht-sniffer.ts
@@ -1,4 +1,3 @@
-const dgram = require("dgram");
 const crypto = require('crypto');
 const DHT = require('bittorrent-dht')
 
@@ -11,16 +10,21 @@ class DHTSniffer extends EventEmitter {
     latest_message_time: Date;
     refresh_interval_id: any;
     rpc: any;
+    status: boolean
     constructor(options) {
         super()
         this._options = Object.assign({
             port: 6881,
             refreshTime: 1 * 60 * 1000,
         }, options);
-        this.initDHT();
+        this.status = false;
     }
-    initDHT() {
+
+    start() {
         const _this = this;
+        if (this.status) {
+            console.log("The sniffer is already working")
+        }
         this.dht = new DHT();
         this.rpc = this.dht._rpc;
         this.dht.listen(this._options.port, () => {
@@ -29,11 +33,12 @@ class DHTSniffer extends EventEmitter {
         this.dht.on("warning", (err) => _this.emit('warning', err));
         this.dht.on("error", (err) => _this.emit('error', err));
         this.dht.on("get_peers", infoHash => {
-            _this.emit('infoHash', infoHash)
+            _this.emit('infoHash', infoHash);
         });
 
         this.dht.on('node', function (node) {
             _this.latest_message_time = new Date();
+            _this.emit('node', node);
             _this.findNode(node, node.id);
         });
         /**
@@ -47,23 +52,25 @@ class DHTSniffer extends EventEmitter {
                 } else {
                     nodes.map(node => {
                         if (Math.random() > 0.5) {
+                            console.log("try find nodes", node);
                             _this.findNode(node, _this.rpc.id);
                         }
                     })
                 }
             }
             console.log("nodes:", nodes.length);
-        }, this._options.refreshTime)
+        }, this._options.refreshTime);
+        this.status = true;
     }
     findNode(node, nid) {
         let id = nid !== undefined ? utils.getNeighborId(nid, this.dht.nodeId) : this.dht.nodeId;
         let message = {
-            t: crypto.randomBytes(2),
+            t: crypto.randomBytes(4),
             y: 'q',
             q: 'find_node',
             a: {
                 id,
-                target: utils.getRandomId()
+                target: crypto.randomBytes(20)
             }
         };
         this.dht._rpc.query(node, message);
